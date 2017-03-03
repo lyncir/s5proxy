631b5bb9232881315fcec8a84066b5b8293f5d94
lyncir <lyncir@gmail.com>
2017-03-03 17:12:52 +0800

    use asyncio,TODO: encrypt tcp connect

--- client.py
+++ client.py
@@ -1,130 +1,99 @@
-# -*- coding: utf8 -*-
-
-import ssl
-import select
-import struct
-import socket
+# -*- coding: utf-8 -*-
 import logging
-import traceback
-import SocketServer
+import socket
+import asyncio
+from struct import pack, unpack
 
-import utils
-
-config = utils.get_config()
-
-def send_all(sock, data):
-    bytes_sent = 0
-    while True:
-        r = sock.send(data[bytes_sent:])
-        if r < 0:
-            return r
-        bytes_sent += r
-        if bytes_sent == len(data):
-            return bytes_sent
+from utils import config
 
 
-def send2server(sock, remote):
-    try:
-        fdset = [sock, remote]
-        while True:
-            r, w, e = select.select(fdset, [], [])
-            if sock in r:
-                data = sock.recv(4096)
-                if len(data) <= 0:
-                    break
-                result = send_all(remote, data)
-                if result < len(data):
-                    raise Exception('failed to send all data')
+class ProxyClient(asyncio.Protocol):
 
-            if remote in r:
-                data = remote.recv(4096)
-                if len(data) <= 0:
-                    break
-                result = send_all(sock, data)
-                if result < len(data):
-                    raise Exception('failed to send all data')
-    finally:
-        sock.close()
-        remote.close()
+    def connection_made(self, transport):
+        self.transport = transport
+        self.server_transport = None
+
+    def data_received(self, data):
+        self.server_transport.write(data)
+
+    def connection_lost(self, exc):
+        self.server_transport.close()
 
 
-class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    allow_reuse_address = True
+class Server(asyncio.Protocol):
+    INIT, HOST, DATA = 0, 1, 2
 
+    def connection_made(self, transport):
+        logging.info('connecting from proxy application: {}'.format(transport.get_extra_info('peername')))
+        # 浏览器和proxyclient的transport
+        self.transport = transport
+        self.state = self.INIT
 
-class Socks5Client(SocketServer.StreamRequestHandler):
+    def connection_lost(self, exc):
+        self.transport.close()
 
-    def handle(self):
-        try:
-            sock = self.connection
-            # client -> server. VER | NMETHODS | METHODS
-            sock.recv(262)
+    def data_received(self, data):
+
+        if self.state == self.INIT:
+            assert data[0] == 0x05
             # server -> client. VER | METHOD
-            sock.send("\x05\x00")
+            self.transport.write(pack('!BB', 0x05, 0x00))  # no auth
+            self.state = self.HOST
+
+        elif self.state == self.HOST:
             # socks request info. VER | CMD | RSV | ATYP | SDT.ADDR | DST.PORT
-            data = self.rfile.read(4) or '\x00' * 4
-            cmd = ord(data[1])
-            if cmd != 1:
-                logging.warn('cmd != 1')
-                return
+            ver, cmd, rsv, atype = data[:4]
+            assert ver == 0x05 and cmd == 0x01
 
-            atyp = ord(data[3])
-            req = data
-            if  atyp == 1:
-                # ipv4
-                addr_ipv4 = self.rfile.read(4)
-                addr = socket.inet_ntoa(addr_ipv4)
-                req += addr_ipv4
-            elif atyp == 3:
-                # domainname
-                addr_len = self.rfile.read(1)
-                addr = self.rfile.read(ord(addr_len))
-                req += addr_len + addr
-            elif atyp == 4:
-                # ipv6
-                addr_ipv6 = self.rfile.read(16)
-                addr = socket.inet_ntop(socket.AF_INET6, addr_ipv6)
-                req += addr_ipv6
+            if atype == 3:    # domain
+                length = data[4]
+                hostname, nxt = data[5:5+length],  5+length
+            elif atype == 1:  # ipv4
+                hostname, nxt = socket.inet_ntop(socket.AF_INET, data[4:8]), 8
+            elif atype == 4:  # ipv6
+                hostname, nxt = socket.inet_ntop(socket.AF_INET6, data[4:20]), 20
             else:
-                logging.warn('addr_type not support')
-                # not support
+                logging.warn('addr_type not suport')
                 return
-            addr_port = self.rfile.read(2)
-            req += addr_port
-            port = struct.unpack('>H', addr_port)
-            try:
-                # socks response info. VER | REP | RSV | ATYP | BND.ADDR | BND.PORT 
-                reply = "\x05\x00\x00\x01"
-                reply += socket.inet_aton('0.0.0.0') + struct.pack('>H', 2222)
-                self.wfile.write(reply)
-                # send request to proxy server. TODO: encrypt
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                remote = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLSv1)
-                remote.connect((config['server'], config['server_port']))
-                remote.send(req)
-                logging.info('connecting %s:%d' % (addr, port[0]))
-            except socket.error, e:
-                logging.warn("connected server %s:%d error. %s" % (config['server'],
-                    config['server_port'], e))
-                return
-            # send and receive. TODO: encrypt
-            send2server(sock, remote)
-        except socket.error, e:
-            logging.warn("socket error: %s" % e)
+
+            port = unpack('!H', data[nxt:nxt+2])[0]
+
+            logging.info('request connect to {}:{}'.format(hostname, port))
+            # 连接proxyserver
+            asyncio.ensure_future(self.connect(hostname, port))
+            self.state = self.DATA
+
+        elif self.state == self.DATA:
+            self.client_transport.write(data)
+
+    async def connect(self, hostname, port):
+        # config
+        server = config['default']['server']
+        server_port = config['default']['server_port']
+
+        loop = asyncio.get_event_loop()
+        # 和proxyclient建立连接
+        transport, client = await loop.create_connection(ProxyClient, server, server_port)
+        # 绑定server_transport和trasport
+        client.server_transport = self.transport
+        self.client_transport = transport
+        # 发送地址信息, 域名和端口
+        self.client_transport.write(
+            pack('!i%ssH' % len(hostname), len(hostname), hostname, port))
 
 
-def main():
+if __name__ == '__main__':
+    # log
     logging.basicConfig(level=logging.DEBUG,
             format='%(asctime)s %(levelname)-8s %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
 
-    LOCAL = config['local']
-    PORT = config['local_port']
+    # config
+    local = config['default']['local']
+    local_port = config['default']['local_port']
 
-    server = ThreadingTCPServer((LOCAL, PORT), Socks5Client)
-    logging.info("starting client at %s:%d" % tuple(server.server_address[:2]))
-    server.serve_forever()
-
-
-if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    srv = loop.create_server(Server, local, local_port)
+    logging.info('start client at {}:{}'.format(local, local_port))
+    loop.run_until_complete(srv)
+    loop.run_forever()
--- server.py
+++ server.py
@@ -1,133 +1,77 @@
-# -*- coding: utf8 -*-
-
-import ssl
-import time
-import select
-import struct
-import socket
+# -*- coding: utf-8 -*-
 import logging
-import traceback
-import SocketServer
+import socket
+import asyncio
+from struct import pack, unpack
 
-import utils
-
-
-config = utils.get_config()
+from utils import config
 
 
-def send_all(sock, data):
-    bytes_sent = 0
-    while True:
-        r = sock.send(data[bytes_sent:])
-        if r < 0:
-            return r
-        bytes_sent += r
-        if bytes_sent == len(data):
-            return bytes_sent
+class Client(asyncio.Protocol):
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.server_transport = None
+
+    def data_received(self, data):
+        self.server_transport.write(data)
+
+    def connection_lost(self, exc):
+        logging.info('connected to {} success!'.format(self.transport.get_extra_info('peername')))
+        self.server_transport.close()
 
 
-def send2server(sock, remote):
-    try:
-        fdset = [sock, remote]
-        while True:
-            r, w, e = select.select(fdset, [], [])
-            if sock in r:
-                data = sock.recv(4096)
-                if len(data) <= 0:
-                    break
-                result = send_all(remote, data)
-                if result < len(data):
-                    raise Exception('failed to send all data')
+class ProxyServer(asyncio.Protocol):
+    INIT, HOST, DATA = 0, 1, 2
 
-            if remote in r:
-                data = remote.recv(4096)
-                if len(data) <= 0:
-                    break
-                result = send_all(sock, data)
-                if result < len(data):
-                    raise Exception('failed to send all data')
-    finally:
-        sock.close()
-        remote.close()
+    def connection_made(self, transport):
+        self.transport = transport
+        self.state = self.INIT
 
+    def connection_lost(self, exc):
+        self.transport.close()
 
-class SSLSocketServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    
-    def get_request(self):
-        newsocket, fromaddr = self.socket.accept()
-        connstream = ssl.wrap_socket(newsocket,
-                server_side=True,
-                certfile=config['certfile'],
-                keyfile=config['keyfile'],
-                ssl_version=ssl.PROTOCOL_TLSv1
-                )
-        return connstream, fromaddr
+    def data_received(self, data):
 
+        if self.state == self.INIT:
+            # 解析proxyclient传过来的第一个包
+            l = unpack('!i', data[:4])[0]
+            _, hostname, port = unpack('!i%ssH' % l, data)
 
-class Socks5Server(SocketServer.StreamRequestHandler):
+            # 与web站点建立连接
+            logging.info('request connect to {}:{}'.format(hostname, port))
+            asyncio.ensure_future(self.connect(hostname, port))
+            self.state = self.DATA
 
-    def handle(self):
-        try:
-            sock = self.connection
-            #data = sock.recv(4096)
-            data = self.rfile.read(4) or '\x00' * 4
+        elif self.state == self.DATA:
+            self.client_transport.write(data)
 
-            cmd = ord(data[1])
-            if cmd != 1:
-                logging.warn('cmd != 1')
-                # send http headers
-                send_all(sock, b"HTTP/1.1 200 OK\r\n")
-                send_all(sock, b"Connection: close\r\n")
-                send_all(sock, b"Content-Type: text/plain\r\n")
-                resp = "Date: " + time.ctime() + "\r\n"
-                send_all(sock, resp.encode('latin-1'))
-                return
+    async def connect(self, hostname, port):
+        loop = asyncio.get_event_loop()
+        # 连接web
+        transport, client = \
+            await loop.create_connection(Client, hostname, port)
+        client.server_transport = self.transport
+        self.client_transport = transport
 
-            atyp = ord(data[3])
-            if  atyp == 1:
-                # ipv4
-                addr_ipv4 = self.rfile.read(4)
-                addr = socket.inet_ntoa(addr_ipv4)
-            elif atyp == 3:
-                # domainname
-                addr_len = self.rfile.read(1)
-                addr = self.rfile.read(ord(addr_len))
-            elif atyp == 4:
-                # ipv6
-                addr_ipv6 = self.rfile.read(16)
-                addr = socket.inet_ntop(socket.AF_INET6, addr_ipv6)
-            else:
-                logging.warn('addr_type not support')
-                # not support
-                return
+        # 返回给浏览器
+        hostip, port = transport.get_extra_info('sockname')
+        host = unpack("!I", socket.inet_aton(hostip))[0]
+        self.transport.write(
+            pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, host, port))
 
-            addr_port = self.rfile.read(2)
-            port = struct.unpack('>H', addr_port)
-
-            try:
-                remote = socket.create_connection((addr, port[0]))
-                logging.info('connecting %s:%d' % (addr, port[0]))
-            except socket.error, e:
-                logging.warn("%s: %s" % (addr, e))
-                return
-            # receive request, send to server
-            send2server(sock, remote)
-        except socket.error, e:
-            logging.warn("socket error: %s" % e)
-
-
-def main():
+if __name__ == '__main__':
+    # log
     logging.basicConfig(level=logging.DEBUG,
             format='%(asctime)s %(levelname)-8s %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
 
-    LOCAL = config['server']
-    PORT = config['server_port']
+    # config
+    server = config['default']['server']
+    server_port = config['default']['server_port']
 
-    server = SSLSocketServer((LOCAL, PORT), Socks5Server)
-    logging.info("starting server at %s:%d" % tuple(server.server_address[:2]))
-    server.serve_forever()
-
-
-if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    srv = loop.create_server(ProxyServer, server, server_port)
+    logging.info('start server at {}:{}'.format(server, server_port))
+    loop.run_until_complete(srv)
+    loop.run_forever()
